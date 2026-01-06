3b17d1f25565f17f1c60bdc8091610335cba1318
SudheerKondamuri <sudheersudheer7654321@gmail.com>
2026-01-06 22:41:09 +0530

    fix: Changed the solidity version to ^0.8.20

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MockERC20 is ERC20 {
     constructor() ERC20("MockERC20", "MERC20") {
