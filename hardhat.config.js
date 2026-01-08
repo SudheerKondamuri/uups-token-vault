02b3ba60e92a26856051dbeddc9cbaa83c8fe13d
SudheerKondamuri <sudheersudheer7654321@gmail.com>
2026-01-08 22:16:17 +0530

    fix: Changed solidity version in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@openzeppelin/hardhat-upgrades");
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+const config = {
   solidity: "0.8.22",
+  networks: {
+    hardhat: {}, 
+  }
 };
+
+export default config;
